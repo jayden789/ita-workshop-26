b2f9111bacea6aedf451568192467d9ab73c7139
Bruce Zhou <bruce.zrc@gmail.com>
2025-02-01 19:17:17 +0000

    Registered all models to django-admin
    Registered all models to django-admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from api.models import User
+from django.apps import apps
 
 class UserAdmin(admin.ModelAdmin):
     search_fields = ['email']
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+
+app = apps.get_app_config('api')
+for model in app.get_models():
+    try:
+        admin.site.register(model)
+    except:
+        pass
